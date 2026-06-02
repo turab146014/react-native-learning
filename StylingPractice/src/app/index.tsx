ffa2a69b3c27e664b81b0262c0005fdab0310b3f
Syed Turab Haider Naqvi <turabhaider621@gmail.com>
2026-06-02 16:03:05 +0500

    Modify  React Native styling

--- StylingPractice/src/app/index.tsx
+++ StylingPractice/src/app/index.tsx
@@ -1,98 +1,58 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {View, Text, StyleSheet } from "react-native";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
+export default function App() {
+  return(
+    <View style = {styles.container}>
+    <View style = {styles.darkMode}>
+      <Text style= {styles.textColor}> Font Color 
+        <Text style = {styles.bold}> On Bold</Text> 
+      </Text>
+    </View>
+    <View style = {[styles.lightbluebg, styles.box, styles.androidShadow]}>
+      <Text> Abdullah </Text>
+    </View>
+    <View style = {[styles.box, styles.graybg, styles.androidShadow]}>
+      <Text>Turab Shah</Text>
+    </View>
+    </View>
   );
 }
-
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor: "white", 
+      padding: 80
+    },
+    darkMode: {
+      backgroundColor : "black", 
+      height : 40,
+      width: 150,
+      color : "white"
+    },
+    textColor: {
+      color : "white",
+    },
+    bold: {
+      fontWeight : "bold"
+    },
+    box: {
+      height : 50,
+      width : 150,
+      margin : 35,
+      borderWidth : 2,
+      borderColor : "black",
+      borderStyle : "solid",
+      borderRadius : 2,
+      backgroundColor : "yellow"
+    },
+    lightbluebg: {
+      backgroundColor : "lightblue"
+    },
+    graybg :{
+      backgroundColor : "gray"
+    },
+    androidShadow: {
+      elevation : 10,
+      shadowColor : "#ff0000"
+    }
+  })
