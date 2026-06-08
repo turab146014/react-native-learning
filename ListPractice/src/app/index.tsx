6527fdd8c511343524ccbd0c2385ba163c5a878b
Syed Turab Haider Naqvi <turabhaider621@gmail.com>
2026-06-08 16:14:00 +0500

    practice: complete React Native list rendering topic

---
+++ ListPractice/src/app/index.tsx
@@ -0,0 +1,165 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  StatusBar,
+  SectionList,
+} from "react-native";
+
+import pokemonList from "../../data.json";
+
+import groupedPokemonList from "../../groupdata.json";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function App() {
+
+  return (
+
+    <SafeAreaView style = {styles.container}>
+
+      {/* <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => {
+          console.log(pokemon.id);
+          return (
+            <View style={styles.card} key={pokemon.id}>
+              <Text style={styles.cardText}>{pokemon.type}</Text>
+              <Text style={styles.cardText}>{pokemon.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView> */}
+
+
+      {/* <View style={styles.scrollView}>
+
+        <FlatList
+          data={pokemonList}
+          renderItem={({ item }) => {
+            console.log(item.id);
+            return (
+              <View style={styles.card}>
+                <Text style={styles.cardText}>{item.type}</Text>
+                <Text style={styles.cardText}>{item.name}</Text>
+              </View>
+            );
+          }}
+
+
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={
+            <View
+              style={{
+                height: 16,
+              }}
+            />
+          }
+
+
+          ListEmptyComponent={<Text>No Items Found</Text>}
+
+          ListHeaderComponent={
+            <Text style={styles.headerText}>Pokemon List</Text>
+          }
+
+          ListFooterComponent={
+            <Text style={styles.footerText}>End of list</Text>
+          }
+
+          // horizontal={true}
+        />
+      </View> */}
+
+      <View style={styles.scrollView}>
+
+
+        <SectionList
+          sections={groupedPokemonList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.cardText}>{item}</Text>
+              </View>
+            );
+          }}
+
+          renderSectionHeader={({ section }) => (
+            <Text style={styles.sectionHeaderText}>{section.type}</Text>
+          )}
+
+
+          ItemSeparatorComponent={() => (
+            <View
+              style={{
+                height: 16,
+              }}
+            />
+          )}
+
+
+          SectionSeparatorComponent={() => (
+            <View
+              style={{
+                height: 16,
+              }}
+            />
+          )}
+        />
+
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    paddingTop: StatusBar.currentHeight,
+  },
+
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    // marginBottom: 16,
+    borderWidth: 1,
+  },
+
+  cardText: {
+    fontSize: 30,
+  },
+
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+
+  footerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 12,
+  },
+
+  sectionHeaderText: {
+    backgroundColor: "#FFFFFF",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  
+});
+
+
+
+
+
+
