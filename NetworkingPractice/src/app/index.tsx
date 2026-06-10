719a3b0aef1f26c9906146c42d5e0888242c0ad7
Syed Turab Haider Naqvi <turabhaider621@gmail.com>
2026-06-10 11:51:04 +0500

    practice: add API posts with loading and refresh

--- NetworkingPractice/src/app/index.tsx
+++ NetworkingPractice/src/app/index.tsx
@@ -2,16 +2,22 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar, StyleSheet, View, FlatList, Text, ActivityIndicator} from "react-native";
 import { useState , useEffect} from "react";
 
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
 export default function App () {
   
-  const [postList, setPostList] = useState ([]);
+  const [postList, setPostList] = useState<Post[]>([]);
 
   const [isLoading, setIsLoading] = useState (true);
 
   const [isRefreshing, setIsRefreshing] = useState (false); 
 
   const fetchData = async (limit = 10) => {
-    const response = await fetch ("https://jsonplaceholder.typicode.com/posts?_limit=10");
+    const response = await fetch ("https://jsonplaceholder.typicode.com/posts?_limit=$(limit)");
     const data = await response.json();
 
     setPostList (data);
@@ -52,6 +58,8 @@ export default function App () {
   <SafeAreaView style = {styles.container}>
 
     <View style = {styles.listContainer}>
+
+      
       <FlatList 
       data={postList}
       renderItem= {({item}) => {
